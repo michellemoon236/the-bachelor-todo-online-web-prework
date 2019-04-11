6ea233bb5ad05ff91f3754595315b3a26dad0e74
Michelle M <michellemoon236@gmail.com>
2019-04-11 00:58:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -57,7 +57,22 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  hometown_array = []
+  #binding.pry
+  data.each do |season_number, contestant_array|
+    #binding.pry
+    contestant_array.each do |contestant|
+      #binding.pry
+      contestant.each do |contestant_attribute, attribute_value|
+      #binding.pry
+        if attribute_value == hometown
+          hometown_array << contestant["name"]
+          #binding.pry
+        end
+      end 
+    end
+  end 
+  hometown_array.length
 end
 
 def get_average_age_for_season(data, season)
