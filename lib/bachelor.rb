50c794f88faf1fb7bde5f9bd84dc865b5fd09ee5
Michelle M <michellemoon236@gmail.com>
2019-04-11 00:29:57 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,40 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  #binding.pry
+  data.each do |season_number, contestant_array|
+    #binding.pry
+    contestant_array.each do |contestant|
+      #binding.pry
+      contestant.each do |contestant_attribute, attribute_value|
+      #binding.pry
+        if season == season_number && contestant_attribute == "status" && attribute_value == "Winner"
+          winner = contestant["name"]
+        end
+      end 
+    end
+  end 
+  winner.split.first
 end
 
+
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  #binding.pry
+  data.each do |season_number, contestant_array|
+    #binding.pry
+    contestant_array.each do |contestant|
+      #binding.pry
+      contestant.each do |contestant_attribute, attribute_value|
+      #binding.pry
+        if contestant_attribute == "occupation" && attribute_value == occupation
+          name = contestant["name"]
+        end
+      end 
+    end
+  end 
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
